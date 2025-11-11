33bf8d240fc613d1a11bcdd28352a6a8780f1e58
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-11 20:27:41 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/$USER && git clone https://github.com/claudemods/vanillaarch-or-cachyos-to-kdedev
-cd /home/$USER/vanillaarch-or-cachyos-to-kdedev && sudo chmod /home/$USER/vanillaarch-or-cachyos-to-kdedev/* && ./commands.sh
+cd /home/$USER/vanillaarch-or-cachyos-to-kdedev && sudo chmod +x /home/$USER/vanillaarch-or-cachyos-to-kdedev/* && ./commands.sh
